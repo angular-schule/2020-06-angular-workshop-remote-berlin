4afb90c1eaf564dd925eba72a185e970c76a45d3
Ferdinand Malcher <ferdinand@malcher.media>
2020-06-11 15:07:21 +0200

    Dashboard mit [object Object]

---
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  books: Book[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.books = [
+      {
+        isbn: '000',
+        title: 'Angular',
+        description: 'Grundlagen und Best Practices',
+        rating: 5
+      },
+      {
+        isbn: '111',
+        title: 'React',
+        description: 'Das andere Framework',
+        rating: 3
+      }
+    ];
+  }
+
+}
