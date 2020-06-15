a2dff92a586228e73447acb804c960e70939b4ed
Ferdinand Malcher <ferdinand@malcher.media>
2020-06-15 11:44:28 +0200

    Events

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
@@ -9,6 +9,8 @@ import { Book } from '../shared/book';
 export class BookComponent implements OnInit {
 
   @Input() book: Book;
+  @Output() rateUp = new EventEmitter<Book>(); // from @angular/core !
+  @Output() rateDown = new EventEmitter<Book>();
 
   constructor() { }
 
@@ -22,6 +24,14 @@ export class BookComponent implements OnInit {
 
   getStars() {
     return new Array(this.book.rating);
+  }
+
+  doRateUp() {
+    this.rateUp.emit(this.book);
+  }
+
+  doRateDown() {
+    this.rateDown.emit(this.book);
   }
 
 }
