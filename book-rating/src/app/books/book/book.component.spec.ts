9696aae08a27d3f7f6a58b726ffa4a4967f39fb2
Ferdinand Malcher <ferdinand@malcher.media>
2020-06-15 14:23:36 +0200

    Test BookComponent

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -1,8 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
+import { Book } from '../shared/book';
 
-describe('BookComponent', () => {
+fdescribe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
 
@@ -16,10 +17,36 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+
+    // Book belegen, denn es gibt ja keine Elternkomponente,
+    // die ein Property Binding macht
+    component.book = {
+      isbn: '',
+      title: '',
+      description: '',
+      price: 5.99,
+      rating: 3
+    };
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit event for doRateUp', () => {
+    // Arrange
+    let emittedBook: Book;
+
+    component.rateUp.subscribe(book => {
+      emittedBook = book;
+    });
+
+    // Act
+    component.doRateUp();
+
+    // Assert
+    expect(emittedBook).toBe(component.book);
   });
 });
