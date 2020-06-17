1e6917df837fb508f54b06bd97532171df9bef4c
Ferdinand Malcher <ferdinand@malcher.media>
2020-06-17 13:02:41 +0200

    Template-Driven Forms

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -19,7 +20,8 @@ import { BookFormComponent } from './book-form/book-form.component';
   ],
   imports: [
     CommonModule,
-    BooksRoutingModule
+    BooksRoutingModule,
+    FormsModule
   ],
   exports: [
     DashboardComponent
